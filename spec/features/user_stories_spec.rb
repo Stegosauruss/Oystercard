9316a5103d51b00e5d52ae622ed9b63c585df2b9
RussellWind <russell.wind@gmail.com>
2019-07-29 14:58:27 +0100

    step 5 completed

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -8,6 +8,13 @@ describe 'User Stories' do
   it "so customers can travel, they can have money on their oystercard" do
     expect(oystercard.balance).not_to eq nil
   end
+
+  # In order to keep using public transport
+  # As a customer
+  # I want to add money to my card
+  it "so that i can travel, I want to add money to my balance" do 
+    expect{oystercard.top_up(5)}.to change{oystercard.balance}.by(5)
+  end
 end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,8 @@ describe Oystercard do
     expect(oystercard.balance).not_to eq nil
   end
 
+  it "tops up" do
+    expect{oystercard.top_up(5)}.to change{oystercard.balance}.by(5)
+  end
+
 end
